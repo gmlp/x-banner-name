a4754f8ec0089536f58acee7e92d6d178b7fb9d2
Oscar <oscar.hernandez@kroger.com>
2019-08-26 15:50:06 -0400

    Initial commit c:

---
+++ README.md
@@ -0,0 +1,6 @@
+Kong plugin 
+====================
+
+This plugin just adds the X-Banner-Name http header to the request, containing the first subdomain of the Host Header.
+
+Based on the kong-plugin (https://github.com/kong/kong-plugin)
---
+++ kong/plugins/krogerplugin/handler.lua
@@ -0,0 +1,36 @@
+-- If you're not sure your plugin is executing, uncomment the line below and restart Kong
+-- then it will throw an error which indicates the plugin is being loaded at least.
+
+--assert(ngx.get_phase() == "timer", "The world is coming to an end!")
+
+
+-- Grab pluginname from module name
+local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+
+-- load the base plugin object and create a subclass
+local plugin = require("kong.plugins.base_plugin"):extend()
+
+-- constructor
+function plugin:new()
+  plugin.super.new(self, plugin_name)
+
+  -- do initialization here, runs in the 'init_by_lua_block', before worker processes are forked
+
+end
+
+---[[ runs in the 'access_by_lua_block'
+function plugin:access(plugin_conf)
+  plugin.super.access(self)
+
+  -- your custom code here
+  string = ngx.req.get_headers()["Host"]
+  subdomain = string:match("([^.]+)")
+  ngx.req.set_header("X-Banner-Name", subdomain)
+
+end --]]
+
+-- set the plugin priority, which determines plugin execution order
+plugin.PRIORITY = 1000
+
+-- return our plugin object
+return plugin
